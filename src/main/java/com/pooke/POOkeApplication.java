597658899559a2f59e4d3ecbdd7e8042cfe36a24
Matheus Miguel <matheusmiguelmedeiros@gmail.com>
2026-05-29 10:42:31 -0300

    setup inicial do projeto maven

---
+++ src/main/java/com/pooke/POOkeApplication.java
@@ -0,0 +1,13 @@
+package com.pooke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class POOkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(POOkeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pooke/POOkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pooke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class POOkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
